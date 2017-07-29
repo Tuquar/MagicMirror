ac5f26fcc15d81174797a2dae06a000e178a597c
Tuquar <mattlennon3@hotmail.co.uk>
2017-07-29 13:38:44 +0100

    init

---
+++ tests/testLibsExist.js
@@ -0,0 +1,12 @@
+
+
+describe('Librarys exist', function(){
+    it('should have requirejs', function(){
+        expect(requirejs).toBeDefined();
+        
+    });
+    it('should be true', function(){
+        expect(true).toBe(true);
+    });
+
+});
